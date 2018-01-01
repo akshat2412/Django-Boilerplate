9c49a9df6f1c0410d649fdda86837c7cec3ed24e
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-01-01 17:44:57 +0530

    changes in urls

--- django_app/app1/urls.py
+++ django_app/app1/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-from . import views
+# from django.conf.urls import include, url
+# from django.contrib import admin
+# from django.urls import path
+# from some_app.views import AboutView
+# from . import views
 
 # urlpatterns = [
 #     url(r'^$', views.post_list, name='post_list'),
+# ]
+
+# urlpatterns = [
+#     path('about/', AboutView.as_view(greetings="hello" and other arguments)),
 # ]
